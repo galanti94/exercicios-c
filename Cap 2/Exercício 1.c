b59ad2f089aeef7014ebf167183f38ca2113f6c3
Galanti <galanti_imp@hotmail.com>
2024-02-12 22:40:33 -0300

    Dois exercícios

---
+++ Cap 2/Exercício 1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+    printf("Início do programa!\n");
+    printf("Fim do programa!\n");
+    return 0;
+}
---
+++ Cap 2/Exercício 2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+    int num;
+    printf("Insira um número inteiro: ");
+    scanf("%d", &num);
+    printf("Você digitou %d \n", num);
+    return 0;
+}
