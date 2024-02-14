ab51cef4d95a5700236a111aca1bd37150e0481a
Galanti <galanti_imp@hotmail.com>
2024-02-14 11:37:29 -0300

    example added

--- Cap 8/typedef.c
+++ Cap 8/typedef.c
@@ -25,8 +25,11 @@ typedef struct {
 int main()
 {
     // Ambas as inicializações resultam na mesma estrutura
-    struct cadastro cad1;
-    cad cad2;
+    struct cadastro cad1 = {"Gui", 29, "Newton Reis", 325};
+    cad cad2 = {"Gui", 29, "Newton Reis", 325};
+
+    printf("%s %d %s %d \n\n", cad1.nome, cad1.idade, cad1.rua, cad1.numero);
+    printf("%s %d %s %d \n\n", cad2.nome, cad2.idade, cad2.rua, cad2.numero);
 
     return 0;
 }
